70eeabf0ee1d1e2f0f916c763b04c22f9ad1e0b8
Jonathan George <Jonathan.George@endjin.com>
2020-03-05 12:12:48 +0000

    Add new method with updated behaviour and marked old method Obsolete

--- Solutions/Corvus.Configuration.TestEnvironment/Corvus/Configuration/ConfigurationExtensions.cs
+++ Solutions/Corvus.Configuration.TestEnvironment/Corvus/Configuration/ConfigurationExtensions.cs
@@ -39,6 +39,7 @@ namespace Corvus.Configuration
         /// }
         /// .
         /// </remarks>
+        [Obsolete("Use AddConfigurationForTest() instead.")]
         public static void AddTestConfiguration(this IConfigurationBuilder builder, string filePath, IDictionary<string, string> fallbackSettings = null)
         {
             if (string.IsNullOrEmpty(filePath))
@@ -55,6 +56,54 @@ namespace Corvus.Configuration
             var codebase = new Uri(typeof(ConfigurationExtensions).Assembly.CodeBase);
             builder.SetBasePath(Path.GetDirectoryName(codebase.LocalPath));
             builder.AddJsonFile(filePath, optional: true, reloadOnChange: true);
+
+            // On the build server, we expect configuration comes through environment variables.
+            builder.AddEnvironmentVariables();
+        }
+
+        /// <summary>
+        /// Adds standard configuration tools for the test environment.
+        /// </summary>
+        /// <param name="builder">The configuration builder to configure.</param>
+        /// <param name="filePath">
+        /// The file path of the local json configuration file to use, relative to the current executing assembly. If there
+        /// is no local json configuration file, this can be set to null.
+        /// </param>
+        /// <param name="fallbackSettings">
+        /// Any fallback settings to apply if not overridden elsewhere. If there are no fallback settings, this can be
+        /// set to null.
+        /// </param>
+        /// <remarks>
+        /// Local json files should contain configuration in either of the following json structures:
+        /// 1. Nested properties
+        /// {
+        ///   "Test": {
+        ///    "Property":  "Value"
+        ///   }
+        /// }
+        /// 2. Flattened, colon separated properties
+        /// {
+        ///  "Test:Property": "Value"
+        /// }
+        /// .
+        /// </remarks>
+        public static void AddConfigurationForTest(
+            this IConfigurationBuilder builder,
+            string filePath = null,
+            IDictionary<string, string> fallbackSettings = null)
+        {
+            if (fallbackSettings != null)
+            {
+                builder.AddInMemoryCollection(fallbackSettings);
+            }
+
+            if (!string.IsNullOrEmpty(filePath))
+            {
+                // Add a JSON file if present for local dev
+                var codebase = new Uri(typeof(ConfigurationExtensions).Assembly.CodeBase);
+                builder.SetBasePath(Path.GetDirectoryName(codebase.LocalPath));
+                builder.AddJsonFile(filePath, optional: true, reloadOnChange: true);
+            }
 
             // On the build server, we expect configuration comes through environment variables.
             builder.AddEnvironmentVariables();
